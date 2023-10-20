5640c3e6bf0d19b26534d4dfa7866ec2ac6ab88f
yusuf umar faruq <luwaservices@gmail.com>
2023-10-19 21:29:00 -0700

    task four

---
+++ Task 4/script.js
@@ -0,0 +1,28 @@
+class BankAccount{
+    constructor(accountNumber, balance){
+        this.accountNumber = accountNumber
+        this.balance = balance
+    }
+
+    depositMoney(cash) {
+        this.balance = cash 
+        console.log(`Account Number: ${this.accountNumber}`)
+        console.log(`Amount Deposited: ${cash}`)
+        console.log(`Account Balance: $${this.balance}`)
+    }
+    withdraw(amount){
+        if(this.balance === 0){
+            console.log("Your account is empty")
+        }else {
+            let newBalance = this.balance - amount
+            this.balance = newBalance
+            console.log(`Amount withdrawn: ${amount}`)
+            console.log(`Account Balance: ${this.balance}`)
+            console.log(`Account Number: ${this.accountNumber}`)
+        }
+    }
+}
+
+const test = new BankAccount(2257281340, 0)
+console.log(test.depositMoney(2000))
+console.log(test.withdraw(500))
