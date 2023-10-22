c867243b6592c77cc8934b6aec492f2c0759b067
yusuf umar faruq <luwaservices@gmail.com>
2023-10-21 20:54:41 -0700

    task 6

---
+++ Task 6/script.js
@@ -0,0 +1,26 @@
+class Book {
+    constructor(title, author, year) {
+        this.title = title
+        this.author = author
+        this.year = year
+    }
+    bookDetails() {
+        console.log(`Title: ${this.title} Year: ${this.year}`)
+        console.log(`Author: ${this.author}`)
+        console.log(`Year: ${this.year}`)
+    }
+}
+
+class Ebook extends Book {
+    constructor(title, author, year, price){
+        super(title, author, year)
+        this.price = price
+    }
+    bookDetails(){
+        super.bookDetails()
+        console.log(`Price: ${this.price}`)
+    }
+}
+
+const book1 = new Ebook("Essential Javascript for beginners", "John Doe", 2021, "$80.20")
+book1.bookDetails()
